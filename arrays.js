c3582c6ed8e25f50619c1546cf411a281e88f31c
Ramya19manda <2211Cs010625@mallareddyuniversity.ac.in>
2025-01-20 13:03:14 +0530

    pushing the files into repository

---
+++ arrays.js
@@ -0,0 +1,17 @@
+// let users=["Ramya","SaiSmitha","Sai nikhil","Rahul","NtR"];
+// let x=users.forEach((user)=>{
+//     return user;
+// });
+// console.log(x);
+
+let userDetails={
+    name:"ram",
+    age:25,
+    city:"hyd"
+
+};
+console.log(userDetails);
+let x=JSON.stringify(userDetails);
+console.log("JSON object "+x);
+let y=JSON.parse(x);
+console.log(y);
