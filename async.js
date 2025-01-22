0082f234e6978b7289f398f0ed622b6cc2889608
Ramya19manda <2211Cs010625@mallareddyuniversity.ac.in>
2025-01-22 13:04:59 +0530

    pushing the files into repository

--- async.js
+++ async.js
@@ -10,14 +10,14 @@
 // console.log(40);
 // console.log(50);
 
- let p1= new Promise((resolve,reject)=>{});
- console.log(p1);
+//  let p1= new Promise((resolve,reject)=>{});
+//  console.log(p1);
 
- let p2=new Promise((resolve,reject)=>{
-     resolve("Success");
-     console.log(p2);  
- });
- console.log(p2); 
+//  let p2=new Promise((resolve,reject)=>{
+//      resolve("Success");
+//      console.log(p2);  
+//  });
+//  console.log(p2); 
 // p2.then((response)=>console.log(response))
 // p2.catch((error)=>console.log(error))
 // p2.finally(()=>console.log("finally for both"))
@@ -28,5 +28,31 @@
 // console.log(p3);
 // p3.then(data=>console.log(data))
 // .catch(err)
+
+function fetchUsers(){
+     let response=fetch("https://jsonplaceholder.typicode.com/users");
+     response.then(result=>{
+        // console.log(res.json());
+        return result.json().then(data=>{
+            console.log(data);
+            let store =document.getElementById("store");
+            console.log(store);
+            data.map(user=>{
+                store.innerHTML += `
+                <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.name}</td>
+                </tr>
+
+                `
+            })
+        })
+    })
+    .catch(err=>console.log(err))
+
+}
+fetchUsers();
 
 
