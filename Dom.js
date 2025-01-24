3729b31a63b0ca5f40b0f82d2eeeeba148349265
Ramya19manda <2211Cs010625@mallareddyuniversity.ac.in>
2025-01-24 12:58:51 +0530

    pushing the files into repo

--- Dom.js
+++ Dom.js
@@ -1,6 +1,43 @@
-console.log(window);
-console.log(document);
-let element=document.grtElementById("demo");
-element.innerText="Go+"
-console.log(element);
+// console.log(window);
+// console.log(document);
+// let element=document.grtElementById("demo");
+// element.innerText="Go+"
+// console.log(element);
+
+// let ele=document.getElementsByClassName("test")
+// console.log(ele);
+// console.log(Array.isArray(ele));
+// ele[0].innerText="Hello"
+// ele[0].style.backgroundColor="orange"
+
+// let x=[...ele];
+// x.map(element=>{
+//     element.style.backgroundColor="tomato"
+// })
+// let ele=document.querySelector(".test");
+// console.log(ele);
+
+// let ele=document.querySelectorAll(".test");
+// console.log(ele);
+
+// let btn=document.querySelector("#btn")
+// btn.addEventListener("click",()=>{
+//     console.log("button clicked");
+//     alert("hi");
+//     document.body.style.backgroundColor="orange";
+
+// })
+// let btn=document.querySelector("#btn")
+// btn.addEventListener("click",()=>{
+//     console.log("button clicked");
+// })
+
+// let divBlock=document.getElementById("divBlock");
+// divBlock.addEventListener("mouseover",()=>{
+//     document.body.style.backgroundColor="yellow";
+    //    divBlock.style.backroungColor="blue";
+    
+// });
+
+
 
