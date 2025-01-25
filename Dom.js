102c726aa063867acf2f069ea0e48287073579ae
Ramya19manda <2211Cs010625@mallareddyuniversity.ac.in>
2025-01-25 12:55:56 +0530

    pushing the files into repo

--- Dom.js
+++ Dom.js
@@ -39,5 +39,47 @@
     
 // });
 
+// let bgColor = document.querySelectorAll(".bgColor");
+// //console.log(bgColor);
+// [...bgColor].map((element)=>{
+//     //console.log(element);
+//     element.addEventListener("mouseover",()=>{
+//         console.log(element.innerText);
+//     })
+// })
+
+// let ele = document.createElement("h1");
+// ele.innetText="Dynamic creation of html element";
+// //ele.setAttribute("id","demo");
+// ele.id="demo";
+// //ele.removeAttribute("id")
+// //console.log(ele);
+
+// let image = document.createElement("img");
+// image.src = "./3606208.jpg";
+// console.log(image);
+// // console.log(ele);
+// document.body.appendChild(ele);
+// document.body.appendChild(image);
+
+
+
+let form = document.querySelector("form");
+let username = document.getElementById("uName");
+let mail = document.getElementById("uMail");
+let psw = document.getElementById("uPass");
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let uname=username.value;
+    let uemail=mail.value;
+    let upass=psw.value;
+    let userDetails={
+        uname,uemail,upass
+    };
+console.log(userDetails);
+localStorage.setItem("userdata",JSON.stringify(userDetails))
+
+});
 
 
